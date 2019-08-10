edd896bcd5d482495767c71744f7043176251c4c
Arif Hidayat <arief21.hidayat21@gmail.com>
2019-08-10 16:19:45 +0700

    #8 user management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -45,7 +45,7 @@
                 <li class="nav-item">
                 <?php endif ?>
 
-                <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                <a class="nav-link pb-0" href="<?= base_url($sm['url']) ?>">
                     <i class="<?= $sm['icon'] ?>"></i>
                     <span><?= $sm['title'] ?></span>
                 </a>
@@ -53,7 +53,7 @@
         <?php endforeach ?>
 
         <!-- Divider -->
-        <hr class="sidebar-divider">
+        <hr class="sidebar-divider mt-3">
 
     <?php endforeach ?>
 
